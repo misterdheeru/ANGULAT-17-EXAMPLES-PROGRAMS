1682cd9200dd1712fea4780fb1bfda4bba0e33e7
mister dheeru <chinthala.dheeru@gmail.com>
2024-07-17 03:55:51 +0530

    update code

--- PROJECT/RNREDDYITSCHOOL/src/app/components/body/body.component.ts
+++ PROJECT/RNREDDYITSCHOOL/src/app/components/body/body.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { CardsComponent } from '../cards/cards.component';
+import { NavbarComponent } from '../navbar/navbar.component';
  
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [CommonModule ,CarouselComponent,CardsComponent],
+  imports: [CommonModule ,CarouselComponent,CardsComponent,NavbarComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
